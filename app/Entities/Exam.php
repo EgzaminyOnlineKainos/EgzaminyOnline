f724ff119c5d879b9a45d13e24ce018f5f21c501
Grzegorz Lieske <grzesiu.lieske@gmail.com>
2016-10-30 18:01:47 +0100

    Exam : getters and setters

--- app/Entities/Exam.php
+++ app/Entities/Exam.php
@@ -51,4 +51,90 @@ class Exam
         $this->tasks = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return Exam
+     */
+    public function setName(string $name): Exam
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStartDate(): string
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param string $startDate
+     *
+     * @return Exam
+     */
+    public function setStartDate(string $startDate): Exam
+    {
+        $this->startDate = $startDate;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEndDate(): string
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param string $endDate
+     *
+     * @return Exam
+     */
+    public function setEndDate(string $endDate): Exam
+    {
+        $this->endDate = $endDate;
+        return $this;
+    }
+
+    /**
+     * @return ExamTask[]
+     */
+    public function getTasks(): array
+    {
+        return $this->tasks;
+    }
+
+    /**
+     * @param ExamTask[] $tasks
+     *
+     * @return Exam
+     */
+    public function setTasks(array $tasks): Exam
+    {
+        $this->tasks = $tasks;
+        return $this;
+    }
+
+
+
 }
