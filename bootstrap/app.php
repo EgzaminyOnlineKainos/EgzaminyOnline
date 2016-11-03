829ee73a02fd2f73aecc9b541f18279f30eed5b1
Adam Kocić <falkan333@gmail.com>
2016-11-03 19:35:45 +0100

    wywalenie mongo z bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,8 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-//$app->register('Jenssegers\Mongodb\MongodbServiceProvider');
-
 /*
 |--------------------------------------------------------------------------
 | Return The Application
